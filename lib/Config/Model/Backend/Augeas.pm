e5f41c1da20896474cf66afd0368ca8c957114fd
Dominique Dumont <dod@debian.org>
2013-05-05 16:29:33 +0200

    prepare release

--- lib/Config/Model/Backend/Augeas.pm
+++ lib/Config/Model/Backend/Augeas.pm
@@ -32,7 +32,7 @@ my $has_augeas = 1;
 eval { require Config::Augeas ;} ;
 $has_augeas = 0 if $@ ;
 
-our $VERSION = '0.113';
+our $VERSION = '0.114';
 
 my $logger =  get_logger('Backend::Augeas') ;
 
