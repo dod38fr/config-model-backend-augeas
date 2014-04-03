09c0665624ed04f651e15d68e0c4ce8ccffbbc02
Dominique Dumont <dod@debian.org>
2014-04-03 18:55:46 +0200

    bumped version and updated changelog

--- lib/Config/Model/Backend/Augeas.pm
+++ lib/Config/Model/Backend/Augeas.pm
@@ -32,7 +32,7 @@ my $has_augeas = 1;
 eval { require Config::Augeas ;} ;
 $has_augeas = 0 if $@ ;
 
-our $VERSION = '0.114';
+our $VERSION = '0.115';
 
 my $logger =  get_logger('Backend::Augeas') ;
 
